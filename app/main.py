bdbd646c0d2000f7f3f5437018aa1a6d84ac6401
Milan Jelisavcic <294258+milanjelisavcic@users.noreply.github.com>
2021-05-20 12:29:11 +0200

    Add file upload page

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from flask import render_template, redirect, url_for
+
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -24,26 +26,19 @@ def respond():
     # Return the response in json format
     return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['doc_file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    """Return the main page."""
+    # stedin_map = get_map()
+    return render_template('index.html', **locals())
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
